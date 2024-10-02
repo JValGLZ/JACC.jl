6ef1539d8c4c7ebb74e3e3bf8f0878aa58752fe9
JValGLZ <108287980+JValGLZ@users.noreply.github.com>
2024-10-02 11:13:55 -0400

    Update JACCCUDA.jl

--- ext/JACCCUDA/JACCCUDA.jl
+++ ext/JACCCUDA/JACCCUDA.jl
@@ -20,9 +20,10 @@ function JACC.parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
     #parallel_kernel = cufunction(_parallel_for_cuda, parallel_tt)
     #maxPossibleThreads = CUDA.maxthreads(parallel_kernel)
     maxPossibleThreads = 512
-    # threads = min(N, maxPossibleThreads)
-    threads = 66407
+    threads = min(N, maxPossibleThreads)
     blocks = ceil(Int, N / threads)
+    threads = 256
+    blocks = 66407
     println("Threads: ", threads, " Blocks: ", blocks)
 
     shmem_size = attribute(device(),CUDA.DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
