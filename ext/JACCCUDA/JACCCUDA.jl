9365fd89d90aead25d66cedb88a13eeaf999541b
jhonnygzz <108152933+jhonnygzz@users.noreply.github.com>
2024-12-09 10:07:08 -0500

    Update JACCCUDA.jl

--- ext/JACCCUDA/JACCCUDA.jl
+++ ext/JACCCUDA/JACCCUDA.jl
@@ -20,10 +20,10 @@ function JACC.parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
     #maxPossibleThreads = CUDA.maxthreads(parallel_kernel)
     maxPossibleThreads = 512
     
-    # threads = min(N, maxPossibleThreads)
-    # blocks = ceil(Int, N / threads)
-    threads = 128
-    blocks = 256
+    threads = min(N, maxPossibleThreads)
+    blocks = ceil(Int, N / threads)
+    # threads = 128
+    # blocks = 256
     # blocks = 66407
     
     println("Threads: ", threads, " Blocks: ", blocks, "N: ", N)
